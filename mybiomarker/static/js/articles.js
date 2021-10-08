cb8f0f6fcc40a23c44a4693dbeec9e318bba58ef
lisapol <epoluboiarinova@gmail.com>
2021-10-08 22:34:52 +0100

    changed font size.

--- mybiomarker/static/js/articles.js
+++ mybiomarker/static/js/articles.js
@@ -58,8 +58,8 @@ function pop() {
        }
     };
 
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "scrollX": true
-    } );
-} );
+//$(document).ready(function() {
+//    $('#example').DataTable( {
+//        "scrollX": true
+//    } );
+//} );
