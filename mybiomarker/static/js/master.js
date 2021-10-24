6d704dc416250d973e2d898cdd41a0251bbd35db
lisapol <epoluboiarinova@gmail.com>
2021-10-24 14:36:22 +0100

    start work on cookies.

--- mybiomarker/static/js/master.js
+++ mybiomarker/static/js/master.js
@@ -88,3 +88,48 @@ $(document).ready(function() {
 	});
 
 });
+
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
+}
+
+const storageType = cookieStorage;
+const consentPropertyName = 'jdc_consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+
+    const acceptFn = event => {
+        saveToStorage(storageType);
+        consentPopup.classList.add('hidden');
+    }
+
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('accept');
+    acceptBtn.addEventListener('click', acceptFn);
+    console.log(shouldShowPopup(storageType));
+
+    if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+//            consentPopup.classList.remove('hidden');
+        document.getElementById('consent-popup').style.display = "none";
+        }, 200);
+    }
+
+};
+
+$( "#accept" ).click(function() {
+  $( "#consent-popup" ).fadeOut( "fast", function() {
+    // Animation complete.
+  });
+});
