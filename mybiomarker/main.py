ea4387db301c6ec7ec924b36f8d2f831b8b921cd
lisapol <epoluboiarinova@gmail.com>
2023-01-15 21:35:01 +0000

    added db

---
+++ mybiomarker/main.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required, current_user
+from mybiomarker import db
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/dashboard')
+@login_required
+def profile():
+    if current_user and current_user.is_authenticated:
+        return redirect('/hello-dashboard')
+
---
+++ mybiomarker/models.py
@@ -0,0 +1,10 @@
+from mybiomarker import db
+# Flask-Login will be able to work with the User model.
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
