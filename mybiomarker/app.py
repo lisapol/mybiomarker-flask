3f4992b938cae900b8c47e24f21677dfb99afe35
Marius <marius.lucian.olariu@gmail.com>
2023-01-16 00:04:50 +0000

    fixed gunicorn 13

--- mybiomarker/app.py
+++ mybiomarker/app.py
@@ -22,9 +22,28 @@ from mybiomarker.models import User
 
 from mybiomarker.data.transform_dataset import transform_blood_profile, transform_menstrual_data
 
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required, current_user
+from mybiomarker.app import db
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/dashboard')
+@login_required
+def profile():
+    if current_user and current_user.is_authenticated:
+        return redirect('/hello-dashboard')
+
+
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-@app.route("/")
+@main.route("/")
 def index():
     return render_template("index.html")
 
